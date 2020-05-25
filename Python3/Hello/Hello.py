7640e644578fe0b629451c795b01caadcdeb3c60
Jishunyi <jishunyi131@163.com>
2020-05-25 15:11:24 +0800

    fileheader

--- Python3/Hello/Hello.py
+++ Python3/Hello/Hello.py
@@ -1,1 +1,9 @@
+'''
+@Author       : Shunyi
+@CreateTime   : Do not edit
+@LastEditors  : Shunyi
+@LastEditTime : 2020-05-25 15:10:53
+@Blog         : https://shunyi.fun/
+@FilePath     : \VSCode\Study\Python3\Hello\Hello.py
+'''
 print("Hello BST")
