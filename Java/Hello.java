7e1ad5b561e705364dcffc778921667b62bb924e
Jishunyi <jishunyi131@163.com>
2020-06-10 09:23:03 +0800

    Java Test

---
+++ Java/Hello.java
@@ -0,0 +1,13 @@
+/*
+ * @Author       : Shunyi
+ * @Date         : 2020-06-10 08:29:05
+ * @LastEditors  : Shunyi
+ * @LastEditTime : 2020-06-10 08:30:54
+ * @Blog         : https://shunyi.fun/
+ * @FilePath     : \VSCode\Study\Java\Hello.java
+ */ 
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Hello Java");
+    }
+}
