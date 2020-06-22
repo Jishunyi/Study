a4d4da646a32e38f1d7ed1e53b11f67eaf16de5a
Jishunyi <jishunyi131@163.com>
2020-06-22 16:39:11 +0800

    atoi

---
+++ C_Primer_Plus_6/Atoi/atoi.c
@@ -0,0 +1,21 @@
+/*
+ * @Author       : Shunyi
+ * @Date         : 2020-06-19 15:12:24
+ * @LastEditors  : Shunyi
+ * @LastEditTime : 2020-06-19 15:16:04
+ * @Blog         : https://shunyi.fun/
+ * @FilePath     : \VSCode\Study\C_Primer_Plus_6\Atoi\atoi.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    char *num = NULL;
+
+    printf("%d\n", atoi(num));
+
+    return 0;
+}
